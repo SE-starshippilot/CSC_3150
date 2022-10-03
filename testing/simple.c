33e65efbea329c2f5b9b5b3e387a3d62c2fbe274
Tianhao <120090472@link.cuhk.edu.cn>
2022-10-03 18:41:37 +0800

    finished configuration of clang-format

--- testing/simple.c
+++ testing/simple.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-int sum(int a, int b){
-    return a+b;
-
-
-
+int sum(int a, int b)
+{
+	return a + b;
 }
-int main(int argc, char* argv[]){
- int a=1 ;
-  int b=2;
-    int c=sum(a, b);
-printf("sum(%d, %d)=%d\n", a, b, c);
-  return 0;
+
+int main(int argc, char *argv[])
+{
+	int a = 2;
+	int b = 2;
+	int c = sum(a, b);
+	printf("sum(%d, %d)=%d\n", a, b, c);
+	return 0;
 }
