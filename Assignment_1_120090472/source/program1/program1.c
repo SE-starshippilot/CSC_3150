2a7c3b5ead269c3538038548cde84b9e3f5d51c7
Tianhao Shi <120090472@link.cuhk.edu.cn>
2022-10-03 15:02:54 +0000

    simplified

--- Assignment_1_120090472/source/program1/program1.c
+++ Assignment_1_120090472/source/program1/program1.c
@@ -5,77 +5,6 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-char* getsig(const int sig){
-    switch (sig)
-    {
-    case 0:
-        return "SIGZERO";
-    case SIGHUP:
-      return "SIGHUP";
-    case SIGINT:
-      return "SIGINT";
-    case SIGQUIT:
-      return "SIGQUIT";
-    case SIGILL:
-      return "SIGILL";
-    case SIGTRAP:
-      return "SIGTRAP";
-    case SIGABRT:
-      return "SIGABRT";
-    case SIGBUS:
-      return "SIGBUS";
-    case SIGFPE:
-      return "SIGFPE";
-    case SIGKILL:
-      return "SIGKILL";
-    case SIGUSR1:
-      return "SIGUSR1";
-    case SIGSEGV:
-      return "SIGSEGV";
-    case SIGUSR2:
-      return "SIGUSR2";
-    case SIGPIPE:
-      return "SIGPIPE";
-    case SIGALRM:
-      return "SIGALRM";
-    case SIGTERM:
-      return "SIGTERM";
-    case SIGSTKFLT:
-      return "SIGSTKFLT";
-    case SIGCHLD:
-      return "SIGCHLD";
-    case SIGCONT:
-      return "SIGCONT";
-    case SIGSTOP:
-      return "SIGSTOP";
-    case SIGTSTP:
-      return "SIGTSTP";
-    case SIGTTIN:
-      return "SIGTTIN";
-    case SIGTTOU:
-      return "SIGTTOU";
-    case SIGURG:
-      return "SIGURG";
-    case SIGXCPU:
-      return "SIGXCPU";
-    case SIGXFSZ:
-      return "SIGXFSZ";
-    case SIGVTALRM:
-      return "SIGVTALRM";
-    case SIGPROF:
-      return "SIGPROF";
-    case SIGWINCH:
-      return "SIGWINCH";
-    case SIGIO:
-      return "SIGIO";
-    case SIGPWR:
-      return "SIGPWR";
-    case SIGSYS:
-      return "SIGSYS";
-    default:
-        return "UNKNOWN SIGNAL";
-    }
-}
 
 int main(int argc, char *argv[]) {
   /* fork a child process */
@@ -112,9 +41,9 @@ int main(int argc, char *argv[]) {
     if (WIFEXITED(status)) {
       printf("Normal termination with EXIT STATUS = %d\n", WEXITSTATUS(status));
     } else if (WIFSTOPPED(status)) {
-      printf("Child process STOPPED by signal %s\n", getsig(WSTOPSIG(status)));
+      printf("Child process STOPPED by signal %d\n", WSTOPSIG(status));
     } else if (WIFSIGNALED(status)) {
-      printf("Child process TERMINATED by signal %s\n", getsig(WTERMSIG(status)));
+      printf("Child process TERMINATED by signal %d\n", WTERMSIG(status));
     } else if (WIFCONTINUED(status)) {
       printf("Child process CONTINUED\n");
     }
