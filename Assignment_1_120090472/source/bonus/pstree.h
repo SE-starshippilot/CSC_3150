9ceaef7fcdf8f797ae1d4a5c43e9b3153dcf3841
tianhao <120090472@link.cuhk..edu.cn>
2022-10-08 20:35:35 +0800

    added struct member

--- Assignment_1_120090472/source/bonus/pstree.h
+++ Assignment_1_120090472/source/bonus/pstree.h
@@ -1,12 +1,25 @@
 #define PROCDIR "/proc"
-#define MAX_CHILD_PROC 256
+#define MAX_NAME_LENGTH 128
+
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <regex.h>
+#include <sys/types.h>
+#include <string>
+#include <iostream>
+
 #include <map>
-typedef struct node{
+typedef struct {
 	pid_t pid;
 	pid_t ppid;
+	std::string name;
 
-	struct node *parent;
-	struct node *childrens[MAX_CHILD_PROC];
+	proc_node *parent;
+    proc_node *first_child;
+    proc_node *next_sibling;
 } proc_node;
+
+void compileRegex(regex_t *regex);
 
 void createProcNode(std::map<int, proc_node*>* p_map, int pid);
