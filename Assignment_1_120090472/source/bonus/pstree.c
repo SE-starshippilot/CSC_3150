2b5fc96e6ccb419e11ba7e065fbb6e730f910224
Tianhao Shi <120090472@link.cuhk.edu.cn>
2022-10-08 12:49:39 +0000

    cleaned makefile

--- Assignment_1_120090472/source/bonus/pstree.c
+++ Assignment_1_120090472/source/bonus/pstree.c
@@ -4,8 +4,21 @@
 #include <sys/types.h>
 #include <dirent.h>
 
+typedef struct {
+    pid_t pid;
+    uid_t uid;
+
+    pid_t ppid;
+    char name[256];
+} proc_t;
+
+
 int main(int argc, char *argv[])
 {
+    /* parse args*/
+    for (int i=1; i<argc; i++) {
+        printf("argument %d is %s\n", i,  argv[i]);
+    }
     DIR *dir;
     dir = opendir(PROCDIR);
     if (dir == NULL) {
@@ -13,5 +26,9 @@ int main(int argc, char *argv[])
         return 1;
     }
     closedir(dir);
+
+    pid_t pid = 0;
+    uid_t uid = 0;
+    
 	return 0;
 }
