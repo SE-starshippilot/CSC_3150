f0e962eb5f739ab01aad68c5e238ed20aabc3749
Tianhao Shi <120090472@link.cuhk.edu.cn>
2022-10-10 03:03:40 +0000

    finished very basic version

--- Assignment_1_120090472/source/bonus/pstree.h
+++ Assignment_1_120090472/source/bonus/pstree.h
@@ -1,7 +1,7 @@
 #define PROCDIR "/proc"
 #define MAX_NAME_LENGTH 128
 #define PROC_FOLDER_PATTERN "^[0-9]+$"
-#define OPSTRING "hV"
+#define OPSTRING "hpV"
 
 #include <dirent.h>
 #include <stdio.h>
@@ -12,6 +12,7 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 #include <map>
 
@@ -24,14 +25,26 @@ const char* verbose =
 "This is free software, and you are welcome to redistribute it under\n"
 "the terms of the GNU General Public License.\n";
 
-struct proc_node{
+struct proc_info{
 	pid_t pid;
 	pid_t ppid;
 	std::string name;
+};
+
+struct proc_node{
+	proc_info info;
 
 	proc_node *parent;
     proc_node *first_child;
     proc_node *next_sibling;
 } ;
 
-void createProcNode(std::map<int, proc_node*>* p_map, int pid);
+void createProcNode(std::map<int, proc_node *> *p_map, proc_info info);
+
+static inline void trim(std::string *s);
+
+proc_info recursiveProcInfo(std::map<int, proc_node *> *p_map, proc_info info);
+
+void insertProcNode(std::map<int, proc_node *> *p_map, int ppid, int pid);
+
+void traverseTree(proc_node *node, int level);
