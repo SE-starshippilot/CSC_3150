bd85cfd44035f428be52d143b457c90868f800ca
Tianhao Shi <120090472@link.cuhk.edu.cn>
2022-10-20 16:02:33 +0000

    first stable ver of bonus

--- Assignment-2/student_assignment2/3150-p2-bonus-main/thread_poll/async.c
+++ Assignment-2/student_assignment2/3150-p2-bonus-main/thread_poll/async.c
@@ -4,12 +4,62 @@
 #include "async.h"
 #include "utlist.h"
 
+#include <stdio.h>
+#include <unistd.h>
+
+my_queue_t *queue;
+
+void *work_container(void *args){
+    while(1){
+        pthread_mutex_lock(&(*queue).lock);
+        while((*queue).size == 0)
+            pthread_cond_wait(&(*queue).not_empty, &(*queue).lock);
+        my_item_t *item = (*queue).head;
+        DL_DELETE((*queue).head, item);
+        (*queue).size--;
+        pthread_mutex_unlock(&(*queue).lock);
+        item->fx(item->args);
+        free(item);
+    }
+}
+
 void async_init(int num_threads) {
+    pthread_t thread;
+    queue = malloc(sizeof(my_queue_t));
+    (*queue).size = 0;
+    (*queue).head = NULL;
+    (*queue).lock = (pthread_mutex_t)PTHREAD_MUTEX_INITIALIZER;
+    (*queue).not_empty = (pthread_cond_t)PTHREAD_COND_INITIALIZER;
+    for (int i = 0; i < num_threads; i++) {
+        pthread_create(&thread, NULL, work_container, NULL);
+        pthread_detach(thread);
+    }
     return;
-    /** TODO: create num_threads threads and initialize the thread pool **/
 }
 
 void async_run(void (*hanlder)(int), int args) {
-    hanlder(args);
-    /** TODO: rewrite it to support thread pool **/
+    pthread_mutex_lock(&(*queue).lock);
+    my_item_t *item = malloc(sizeof(my_item_t));
+    item->fx = hanlder;
+    item->args = args;
+    DL_APPEND((*queue).head, item);
+    (*queue).size++;
+    pthread_mutex_unlock(&(*queue).lock);
+    pthread_cond_signal(&(*queue).not_empty);
+}
+
+void very_slow_add(int num){
+    int result = num + 10;
+    sleep(2);
+    printf("%d + %d = %d\n", num, 10, result);
+}
+
+
+int main(){
+    async_init(10);
+    for (int i=1; i<60; i++){
+        async_run(very_slow_add, i);
+    }
+    pthread_exit(NULL);
+    return 0;
 }
--- Assignment-2/student_assignment2/3150-p2-bonus-main/thread_poll/async.h
+++ Assignment-2/student_assignment2/3150-p2-bonus-main/thread_poll/async.h
@@ -3,9 +3,11 @@
 
 #include <pthread.h>
 
+typedef void (*worker_func)(int args); // the ACTUAL function that will be executed
 
 typedef struct my_item {
-  /* TODO: More stuff here, maybe? */
+  worker_func fx;         // the function to be executed
+  int args;             // the arguments of that function
   struct my_item *next;
   struct my_item *prev;
 } my_item_t;
@@ -13,8 +15,10 @@ typedef struct my_item {
 typedef struct my_queue {
   int size;
   my_item_t *head;
-  /* TODO: More stuff here, maybe? */
+  pthread_mutex_t lock;       // mutex lock
+  pthread_cond_t not_empty;   // conditional var
 } my_queue_t;
+
 
 void async_init(int);
 void async_run(void (*fx)(int), int args);
