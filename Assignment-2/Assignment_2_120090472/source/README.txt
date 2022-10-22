645e9f8a24bdb13efa7db68b9986d819beb3856f
Tianhao <120090472@link.cuhk.edu.cn>
2022-10-22 16:59:33 +0800

    added items for assignment 2

---
+++ Assignment-2/Assignment_2_120090472/source/README.txt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:060ce87e1d21887a20d6d59fe141c7e6523be5cc600041e5d9bb4c4811a5a6ea
+size 230
---
+++ Assignment-2/Assignment_2_120090472/source/hw2.h
@@ -0,0 +1,10 @@
+#ifndef _HW2_H_
+#define _HW2_H_
+
+void draw_map();
+void initialize_logs();
+void update_game_status();
+void* logs_move(void*);
+void* frog_move(void*);
+
+#endif
