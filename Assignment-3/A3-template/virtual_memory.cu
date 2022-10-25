c9efeca73079b4f10cfe0674e4fd8bc3044e1763
Tianhao <120090472@link.cuhk.edu.cn>
2022-10-25 10:34:28 +0800

    added a3

---
+++ Assignment-3/A3-template/virtual_memory.cu
@@ -0,0 +1,49 @@
+﻿#include "virtual_memory.h"
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+__device__ void init_invert_page_table(VirtualMemory *vm) {
+
+  for (int i = 0; i < vm->PAGE_ENTRIES; i++) {
+    vm->invert_page_table[i] = 0x80000000; // invalid := MSB is 1
+    vm->invert_page_table[i + vm->PAGE_ENTRIES] = i;
+  }
+}
+
+__device__ void vm_init(VirtualMemory *vm, uchar *buffer, uchar *storage,
+                        u32 *invert_page_table, int *pagefault_num_ptr,
+                        int PAGESIZE, int INVERT_PAGE_TABLE_SIZE,
+                        int PHYSICAL_MEM_SIZE, int STORAGE_SIZE,
+                        int PAGE_ENTRIES) {
+  // init variables
+  vm->buffer = buffer;
+  vm->storage = storage;
+  vm->invert_page_table = invert_page_table;
+  vm->pagefault_num_ptr = pagefault_num_ptr;
+
+  // init constants
+  vm->PAGESIZE = PAGESIZE;
+  vm->INVERT_PAGE_TABLE_SIZE = INVERT_PAGE_TABLE_SIZE;
+  vm->PHYSICAL_MEM_SIZE = PHYSICAL_MEM_SIZE;
+  vm->STORAGE_SIZE = STORAGE_SIZE;
+  vm->PAGE_ENTRIES = PAGE_ENTRIES;
+
+  // before first vm_write or vm_read
+  init_invert_page_table(vm);
+}
+
+__device__ uchar vm_read(VirtualMemory *vm, u32 addr) {
+  /* Complate vm_read function to read single element from data buffer */
+  return 123; //TODO
+}
+
+__device__ void vm_write(VirtualMemory *vm, u32 addr, uchar value) {
+  /* Complete vm_write function to write value into data buffer */
+}
+
+__device__ void vm_snapshot(VirtualMemory *vm, uchar *results, int offset,
+                            int input_size) {
+  /* Complete snapshot function togther with vm_read to load elements from data
+   * to result buffer */
+}
+
