1a444c5c7abf492bd17aa4d115f8cd6bed3d9f72
Danny SHI <120090472@link.cuhk.edu.cn>
2022-11-09 01:42:24 +0800

    fixed LRU update bug

--- Assignment-3/A3-template/virtual_memory.cu
+++ Assignment-3/A3-template/virtual_memory.cu
@@ -17,41 +17,35 @@ __device__ void init_swap_table(VirtualMemory* vm) {
     vm->swap_table[i] = -1; // invalid :-1
   }
 }
-__device__ void show_lru(VirtualMemory* vm){
-  if (vm->lru_size < 8){
-    for(struct LRUNode* _tmp = vm->lru_head; _tmp!=NULL; _tmp=_tmp->next){
-      printf("%d->", _tmp->page_id);
-    }
+__device__ void show_lru(VirtualMemory* vm) {
+  // int size = 4;
+  struct LRUNode* _tmp = vm->lru_head;
+  while (_tmp != NULL) {
+    printf("%d->", _tmp->page_id);
+    _tmp = _tmp->next;
   }
-  else{
-    int size = 4;
-    struct LRUNode* _tmp = vm->lru_head;
-    for(int i=0; i<size; i++){
-      printf("%d->", _tmp->page_id);
-      _tmp = _tmp->next;
-    }
-    printf("...->");
-    _tmp = vm->lru_tail;
-    for(int i=0; i<size; i++){
-      printf("%d<-", _tmp->page_id);
-      _tmp = _tmp->prev;
-    }
-    printf("\n");
-  }
+  printf("\n");
 }
+
 __device__ int update_lru(VirtualMemory* vm, int page_id, int is_oldpage) {
   /* If lru recorded the page, find it and place it at the top of LRU array */
-  int ret_val = -1;  // index of page_id in LRU array
+  printf("Trying to put %d in list.Before update:", page_id);
   show_lru(vm);
+  int ret_val = -1;  // index of page_id in LRU array
   if (is_oldpage) {                  // save some time. no need to search LRU array if it is a new page
     for (LRUNode* i = vm->lru_head; i != NULL; i = i->next) {
       if (i->page_id == page_id) {
-        i->prev->next = i->next;
+        if (i->prev != NULL)i->prev->next = i->next;
         if (i->next != NULL)i->next->prev = i->prev;
-        else vm->lru_tail = vm->lru_tail->prev;
+        else {
+          vm->lru_tail = i->prev;
+          vm->lru_tail->next = NULL;
+        }
+        i->next = vm->lru_head;
         i->prev = NULL;
-        i->next = (vm->lru_head);
-        (vm->lru_head) = i;
+        vm->lru_head->prev = i;
+        vm->lru_head = i;
+        break;
       }
     }//find
   }
@@ -60,20 +54,22 @@ __device__ int update_lru(VirtualMemory* vm, int page_id, int is_oldpage) {
     _node->page_id = page_id;
     _node->next = vm->lru_head;
     _node->prev = NULL;
-    if (vm->lru_size==0)vm->lru_tail = _node;
+    if (vm->lru_size == 0)vm->lru_tail = _node;
     else vm->lru_head->prev = _node;
     vm->lru_head = _node;
     vm->lru_size++;
+    printf("Current LRU size:%d", vm->lru_size);
     if (vm->lru_size > vm->PAGE_ENTRIES) {
-      struct LRUNode* del_node = vm->lru_tail;
-      ret_val = del_node->page_id;
+      printf("Current tail, page_id is %d, prev is %p, next is %p\n", vm->lru_tail->page_id, vm->lru_tail->prev, vm->lru_tail->next);
+      ret_val = vm->lru_tail->page_id;
       vm->lru_tail = vm->lru_tail->prev;
-      vm->lru_tail->next=NULL;
-      del_node->prev=NULL;
-      free(del_node);
+      vm->lru_tail->next = NULL;
       vm->lru_size--;
+      printf("After update, tail page_id is %d, prev is %p, next is %p\n", vm->lru_tail->page_id, vm->lru_tail->prev, vm->lru_tail->next);
     }
   }
+  // printf("After update:");
+  // show_lru(vm);
   return ret_val;
 }
 
@@ -154,35 +150,43 @@ __device__ uchar vm_read(VirtualMemory* vm, u32 addr) {
   int page_id = addr / vm->PAGESIZE;
   int page_offset = addr % vm->PAGESIZE;
   int physical_addr;
-
+  printf("[Read] VA: %d Pid:%d Poff:%d ", addr, page_id, page_offset);
   PageTableQuery res = query_page_table(vm, page_id);
   if (res.frame_id != -1) {
     physical_addr = res.frame_id * vm->PAGESIZE + page_offset;
     if (vm->lru_head->page_id != page_id) update_lru(vm, page_id, 1);//get the least recently used page as victim
+    printf("[HIT!!:%d] Fid: %d ", (*vm->pagefault_num_ptr), res.frame_id);
   }
   else {
     (*vm->pagefault_num_ptr)++;
+    printf("[FAULT:%d]", (*vm->pagefault_num_ptr));
     int victim_page_id = update_lru(vm, page_id, 0);
     if (res.empty_frame != -1) {
+      printf(" [Empty Frame:%d]", res.empty_frame);
       vm->invert_page_table[res.empty_frame] = page_id;
+      printf("Fid: %d ", res.empty_frame);
       physical_addr = res.empty_frame * vm->PAGESIZE + page_offset;
     }
     else {//the RAM is full and the requested page is in the disk
+      printf("Victim frame:%d", victim_page_id);
       uchar temp_frame[32];
       PageTableQuery victim_res = query_page_table(vm, victim_page_id);
+      printf("Found victim on frame:%d", victim_res.frame_id);
       assert(victim_res.frame_id != -1);
       for (int i = 0; i < vm->PAGESIZE; i++) { // transfer the frame to the buffer
         int _physical_addr = victim_res.frame_id * vm->PAGESIZE + i;
         temp_frame[i] = vm->buffer[_physical_addr];
       }
 
-      int target_disk_page_id = search_swap_table(vm, page_id);
-      swap_page(vm, target_disk_page_id, victim_page_id, temp_frame);
+      int target_disk_frame_id = search_swap_table(vm, page_id);
+      printf("Found target page on disk frame:%d", target_disk_frame_id);
+      swap_page(vm, target_disk_frame_id, victim_page_id, temp_frame);
       vm->invert_page_table[victim_res.frame_id] = page_id;
+      printf("Fid: %d ", victim_res.frame_id);
       physical_addr = victim_res.frame_id * vm->PAGESIZE + page_offset;
     }
   }
-  printf("Reading %d from %d\n", vm->buffer[physical_addr], physical_addr);
+  printf("Val %d PA %d\n", vm->buffer[physical_addr], physical_addr);
   return vm->buffer[physical_addr];
 }
 
@@ -191,17 +195,20 @@ __device__ void vm_write(VirtualMemory* vm, u32 addr, uchar value) {
   int page_id = addr / vm->PAGESIZE;
   int page_offset = addr % vm->PAGESIZE;
   int physical_addr;
-
+  printf("[Write]Val: %d VA: %d Pid:%d Poff:%d ", value, addr, page_id, page_offset);
   PageTableQuery res = query_page_table(vm, page_id);
   if (res.frame_id != -1) {
     physical_addr = res.frame_id * vm->PAGESIZE + page_offset;
     if (vm->lru_head->page_id != page_id) update_lru(vm, page_id, 1);
+    printf("[HIT!!:%d] Fid: %d ", (*vm->pagefault_num_ptr), res.frame_id);
   }
   else {
     (*vm->pagefault_num_ptr)++; //gotta dereference the pointer first!
+    printf("[FAULT:%d]", (*vm->pagefault_num_ptr));
     int victim_page_id = update_lru(vm, page_id, 0);//if it's not in page table, it must not be in LRU. pick the victim
     if (res.empty_frame != -1) { //there are still vacant space in the RAM
       vm->invert_page_table[res.empty_frame] = page_id; //update inverted page table, SET valid bit
+      printf("Fid: %d ", res.empty_frame);
       physical_addr = res.empty_frame * vm->PAGESIZE + page_offset;
     }
     else {                      //no more vacant space in the RAM, requires swapping
@@ -213,13 +220,14 @@ __device__ void vm_write(VirtualMemory* vm, u32 addr, uchar value) {
         temp_frame[i] = vm->buffer[_physical_addr];
       }
 
-      int target_disk_page_id = search_swap_table(vm, page_id);
-      swap_page(vm, target_disk_page_id, victim_page_id, temp_frame);
+      int target_disk_frame_id = search_swap_table(vm, page_id);
+      swap_page(vm, target_disk_frame_id, victim_page_id, temp_frame);
       vm->invert_page_table[victim_res.frame_id] = page_id;
-      physical_addr = victim_page_id * vm->PAGESIZE + page_offset;
+      printf("Fid: %d ", victim_res.frame_id);
+      physical_addr = victim_res.frame_id * vm->PAGESIZE + page_offset;
     }
   }
-  printf("Writing %d to %d. Current LRU size:%d.\n", value, physical_addr, vm->lru_size);
+  printf("PA %d. LRU size:%d.\n", physical_addr, vm->lru_size);
   vm->buffer[physical_addr] = value;
 }
 
@@ -227,8 +235,10 @@ __device__ void vm_snapshot(VirtualMemory* vm, uchar* results, int offset,
   int input_size) {
   /* Complete snapshot function togther with vm_read to load elements from data
    * to result buffer */
-  for (int i = offset; i < input_size; ++i) {
-    results[i] = vm_read(vm, i);
+  for (int i = offset; i < input_size; i++) {
+    int read_val = vm_read(vm, i);
+    // printf("Read val: %d\n", read_val);
+    results[i] = read_val;
   }
 }
 
