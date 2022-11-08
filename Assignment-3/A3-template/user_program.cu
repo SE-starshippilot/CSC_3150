78738d82b1f6923ef7b399c1e8a0677e44f1383e
Danny SHI <120090472@link.cuhk.edu.cn>
2022-11-09 05:26:58 +0800

    modified user program

--- Assignment-3/A3-template/user_program.cu
+++ Assignment-3/A3-template/user_program.cu
@@ -4,16 +4,15 @@
 #include <stdio.h>
 
 __device__ void user_program(VirtualMemory *vm, uchar *input, uchar *results,
-                             int input_size) {
-  // input_size = 65535;
-  for (int i = 0; i < input_size; i++){
-    vm_write(vm, i, input[i]);
-  }
-
-  for (int i = input_size - 1; i >= input_size - 32769; i--){
-    int value = vm_read(vm, i);
-    printf("Attempting to read value %d from %d.\n", value, i);
-  }
-
-  vm_snapshot(vm, results, 0, input_size);
+  int input_size) {
+// write the data.bin to the VM starting from address 32*1024
+for (int i = 0; i < input_size; i++)
+  vm_write(vm, 32*1024+i, input[i]);
+// write (32KB-32B) data  to the VM starting from 0
+for (int i = 0; i < 32*1023; i++)
+  vm_write(vm, i, input[i+32*1024]);
+// readout VM[32K, 160K] and output to snapshot.bin, which should be the same with data.bin
+vm_snapshot(vm, results, 32 *1024, input_size);
 }
+
+// expected page fault num: 9215
