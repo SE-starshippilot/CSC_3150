ee70bbbbb37ddad5905d00c85cebb10efd9c4709
Danny SHI <120090472@link.cuhk.edu.cn>
2022-11-27 19:44:17 +0800

    fixed bugs

--- Assignment-4/source/file_system.h
+++ Assignment-4/source/file_system.h
@@ -63,4 +63,5 @@ __device__ int has_enough_space(FileSystem* fs, int block_size);
 __device__ int move_file(FileSystem* fs, u32 fp, int new_start_block_idx);
 __device__ int fs_compress(FileSystem* fs);
 __device__ u32 fs_allocate(FileSystem* fs, int block_num);
+__device__ void fs_diagnose(FileSystem* fs, u32 fp);
 #endif
