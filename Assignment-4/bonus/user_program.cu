ba854262ebcb7f789e32476d0afdc42de0dd24c0
Danny SHI <120090472@link.cuhk.edu.cn>
2022-11-29 14:56:52 +0800

    fixed PWD

--- Assignment-4/bonus/user_program.cu
+++ Assignment-4/bonus/user_program.cu
@@ -1,72 +1,10 @@
 ﻿#include "file_system.h"
 #include <cuda.h>
 #include <cuda_runtime.h>
-#include <stdio.h>
-__device__ void user_program(FileSystem* fs, uchar* input, uchar* output) {
 
-	/*
-	/////////////// Test Case 1  ///////////////
-	u32 fp = fs_open(fs, "t.txt\0", G_WRITE);
-	fs_write(fs, input, 64, fp);
-	fp = fs_open(fs, "b.txt\0", G_WRITE);
-	fs_write(fs, input + 32, 32, fp);
-	fp = fs_open(fs, "t.txt\0", G_WRITE);
-	fs_write(fs, input + 32, 32, fp);
-	fp = fs_open(fs, "t.txt\0", G_READ);
-	fs_read(fs, output, 32, fp);
-	fs_gsys(fs,LS_D);
-	fs_gsys(fs, LS_S);
-	fp = fs_open(fs, "b.txt\0", G_WRITE);
-	fs_write(fs, input + 64, 12, fp);
-	fs_gsys(fs, LS_S);
-	fs_gsys(fs, LS_D);
-	fs_gsys(fs, RM, "t.txt\0");
-	fs_gsys(fs, LS_S);
-	*/
 
-	/*
-	/////////////// Test Case 2  ///////////////
-	u32 fp = fs_open(fs, "t.txt\0", G_WRITE);
-	fs_write(fs,input, 64, fp);
-	fp = fs_open(fs,"b.txt\0", G_WRITE);
-	fs_write(fs,input + 32, 32, fp);
-	fp = fs_open(fs,"t.txt\0", G_WRITE);
-	fs_write(fs,input + 32, 32, fp);
-	fp = fs_open(fs,"t.txt\0", G_READ);
-	fs_read(fs,output, 32, fp);
-	fs_gsys(fs,LS_D);
-	fs_gsys(fs,LS_S);
-	fp = fs_open(fs,"b.txt\0", G_WRITE);
-	fs_write(fs,input + 64, 12, fp);
-	fs_gsys(fs,LS_S);
-	fs_gsys(fs,LS_D);
-	fs_gsys(fs,RM, "t.txt\0");
-	fs_gsys(fs,LS_S);
-	char fname[10][20];
-	for (int i = 0; i < 10; i++)
-	{
-		fname[i][0] = i + 33;
-		for (int j = 1; j < 19; j++)
-			fname[i][j] = 64 + j;
-		fname[i][19] = '\0';
-	}
-
-	for (int i = 0; i < 10; i++)
-	{
-		fp = fs_open(fs,fname[i], G_WRITE);
-		fs_write(fs,input + i, 24 + i, fp);
-	}
-
-	fs_gsys(fs,LS_S);
-
-	for (int i = 0; i < 5; i++)
-		fs_gsys(fs,RM, fname[i]);
-
-	fs_gsys(fs,LS_D);
-	*/
-
-	/*
-	/////////////// Test Case 3  ///////////////
+__device__ void user_program(FileSystem *fs, uchar *input, uchar *output) {
+	/////////////////////// Bonus Test Case ///////////////
 	u32 fp = fs_open(fs, "t.txt\0", G_WRITE);
 	fs_write(fs, input, 64, fp);
 	fp = fs_open(fs, "b.txt\0", G_WRITE);
@@ -77,116 +15,39 @@ __device__ void user_program(FileSystem* fs, uchar* input, uchar* output) {
 	fs_read(fs, output, 32, fp);
 	fs_gsys(fs, LS_D);
 	fs_gsys(fs, LS_S);
-	fp = fs_open(fs, "b.txt\0", G_WRITE);
-	fs_write(fs, input + 64, 12, fp);
-	fs_gsys(fs, LS_S);
+	fs_gsys(fs, MKDIR, "app\0");
 	fs_gsys(fs, LS_D);
-	fs_gsys(fs, RM, "t.txt\0");
 	fs_gsys(fs, LS_S);
+	fs_gsys(fs, CD, "app\0");
+	fs_diagnose(fs);
+	// fs_gsys(fs, LS_S);
+	// fp = fs_open(fs, "a.txt\0", G_WRITE);
+	// fs_write(fs, input + 128, 64, fp);
+	// fp = fs_open(fs, "b.txt\0", G_WRITE);
+	// fs_write(fs, input + 256, 32, fp);
+	// fs_gsys(fs, MKDIR, "soft\0");
+	// fs_gsys(fs, LS_S);
+	// fs_gsys(fs, LS_D);
+	// fs_gsys(fs, CD, "soft\0");
+	// fs_gsys(fs, PWD);
+	// fp = fs_open(fs, "A.txt\0", G_WRITE);
+	// fs_write(fs, input + 256, 64, fp);
+	// fp = fs_open(fs, "B.txt\0", G_WRITE);
+	// fs_write(fs, input + 256, 1024, fp);
+	// fp = fs_open(fs, "C.txt\0", G_WRITE);
+	// fs_write(fs, input + 256, 1024, fp);
+	// fp = fs_open(fs, "D.txt\0", G_WRITE);
+	// fs_write(fs, input + 256, 1024, fp);
+	// fs_gsys(fs, LS_S);
+	// fs_gsys(fs, CD_P);
+	// fs_gsys(fs, LS_S);
+	// fs_gsys(fs, PWD);
+	// fs_gsys(fs, CD_P);
+	// fs_gsys(fs, LS_S);
+	// fs_gsys(fs, CD, "app\0");
+	// fs_gsys(fs, RM_RF, "soft\0");
+	// fs_gsys(fs, LS_S);
+	// fs_gsys(fs, CD_P);
+	// fs_gsys(fs, LS_S);
 
-	char fname[10][20];
-	for (int i = 0; i < 10; i++)
-	{
-		fname[i][0] = i + 33;
-		for (int j = 1; j < 19; j++)
-			fname[i][j] = 64 + j;
-		fname[i][19] = '\0';
-	}
-
-	for (int i = 0; i < 10; i++)
-	{
-		fp = fs_open(fs, fname[i], G_WRITE);
-		fs_write(fs, input + i, 24 + i, fp);
-	}
-
-	fs_gsys(fs, LS_S);
-
-	for (int i = 0; i < 5; i++)
-		fs_gsys(fs, RM, fname[i]);
-	fs_gsys(fs, LS_D);
-
-	char fname2[1018][20];
-	int p = 0;
-
-	// How the naming works:
-	// 1. k controls the file name length(ranging from 2 to 14, inclusive)
-	// 2. i controls the first character of the file name(ranging from 50->[ASCII=2] to 126->[ASCII=~], inclusive)
-	// 3. j controls other character of the file name(ranging from 1 to k-1, inclusive)
-	
-	// for every k there are 76 files with different first character and the same length
-	// 2A -> 3A -> 4A -> ... -> ~A -> 2AB -> 3AB -> 4AB -> ... -> ~AB -> 2ABC -> ... -> ~ABCDEFGHIJKLM
-	// |<-----------76---------->|     |<-----------76---------->|     
-
-	for (int k = 2; k < 15; k++)
-		for (int i = 50; i <= 126; i++, p++)
-		{
-			fname2[p][0] = i;
-			for (int j = 1; j < k; j++)
-				fname2[p][j] = 64 + j;
-			fname2[p][k] = '\0';
-		}
-	//each file in fname2 is of size 24+i bytes, where i is the index of the file in fname2. The content starts from the ith byte of input
-	for (int i = 0; i < 1001; i++)
-	{
-		fp = fs_open(fs, fname2[i], G_WRITE);
-		fs_write(fs, input + i, 24 + i, fp);
-	}
-	fp = fs_open(fs, fname2[1000], G_READ);
-	fs_read(fs, output + 1000, 1024, fp);
-
-	char fname3[17][3];
-	for (int i = 0; i < 17; i++)
-	{
-		fname3[i][0] = 97 + i;
-		fname3[i][1] = 97 + i;
-		fname3[i][2] = '\0';
-		fp = fs_open(fs, fname3[i], G_WRITE);
-		fs_write(fs, input + 1024 * i, 1024, fp);
-	}
-
-	fp = fs_open(fs, "EA\0", G_WRITE);
-	fs_write(fs, input + 1024 * 100, 1024, fp);
-	fs_gsys(fs, LS_S);
-	*/
-
-	/////////////// Test Case 4  ///////////////
-	///*
-    u32 fp = fs_open(fs, "32-block-0", G_WRITE);
-    fs_write(fs, input, 32, fp);
-    for (int j = 0; j < 1023; ++j) {
-        char tag[] = "1024-block-????";
-        int i = j;
-        tag[11] = static_cast<char>(i / 1000 + '0');
-        i = i % 1000;
-        tag[12] = static_cast<char>(i / 100 + '0');
-        i = i % 100;
-        tag[13] = static_cast<char>(i / 10 + '0');
-        i = i % 10;
-        tag[14] = static_cast<char>(i + '0');
-        fp = fs_open(fs, tag, G_WRITE);
-        fs_write(fs, input + j * 1024, 1024, fp);
-    }
-    fs_gsys(fs, RM, "32-block-0");
-    // now it has one 32byte at first, 1023 * 1024 file in the middle
-
-    fp = fs_open(fs, "1024-block-1023", G_WRITE);
-    printf("triggering gc\n");
-    fs_write(fs, input + 1023 * 1024, 1024, fp);
-
-
-    fs_gsys(fs, LS_D);
-    for (int j = 0; j < 1024; ++j) {
-        char tag[] = "1024-block-????";
-        int i = j;
-        tag[11] = static_cast<char>(i / 1000 + '0');
-        i = i % 1000;
-        tag[12] = static_cast<char>(i / 100 + '0');
-        i = i % 100;
-        tag[13] = static_cast<char>(i / 10 + '0');
-        i = i % 10;
-        tag[14] = static_cast<char>(i + '0');
-        fp = fs_open(fs, tag, G_READ);
-        fs_read(fs, output + j * 1024, 1024, fp);
-    }
-	//*/
 }
