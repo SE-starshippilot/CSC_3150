bf0b47dbb0b020fed8abe5d2b383bd212ac89208
Danny SHI <120090472@link.cuhk.edu.cn>
2023-01-02 22:25:00 +0800

    cleared A1 files

--- Assignment-1/Assignment_1_120090472/source/ReadMe.txt
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2dada0a3a5e1badcee5ea76360145b6c049975306fed5338d4e2f158b513b9a2
-size 2167
--- Assignment-1/Assignment_1_120090472/source/program1/normal.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(int argc,char* argv[]){
-	printf("------------CHILD PROCESS START------------\n");
-	printf("This is the normal program\n\n");
-	printf("------------CHILD PROCESS END------------\n");
-	return 0;
-}
